b96a0e79c6538544ce9d5d6bd2ebb7d7ba232677
JavaJosh <github@javajosh.com>
2021-11-08 11:47:36 -0500

    Experiments with Java network enumeration

--- pim-server-java/src/main/java/com/javajosh/pim/Main.java
+++ pim-server-java/src/main/java/com/javajosh/pim/Main.java
@@ -1,7 +1,30 @@
 package com.javajosh.pim;
 
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello");
+    Enumeration<NetworkInterface> interfaces;
+    try {
+      interfaces = NetworkInterface.getNetworkInterfaces();
+    } catch (SocketException ex) {
+      System.out.println("Operation failed.");
+      ex.printStackTrace();
+      return;
+    }
+
+    while (interfaces.hasMoreElements()) {
+      NetworkInterface networkInterface = interfaces.nextElement();
+      Enumeration<InetAddress> inetAddresses = networkInterface.getInetAddresses();
+      while (inetAddresses.hasMoreElements()) {
+        InetAddress inetAddress = inetAddresses.nextElement();
+        System.out.format("interface %s with address %s%n",
+          networkInterface.getDisplayName(), inetAddress.getHostAddress());
+
+      }
+    }
   }
 }
