4843fb48ee270108646030e60f5eb4865c19f4ab
JavaJosh <github@javajosh.com>
2021-11-08 11:48:58 -0500

    Explanitory bullshit

--- pim-server-java/src/main/java/com/javajosh/pim/InventoryApplication.java
+++ pim-server-java/src/main/java/com/javajosh/pim/InventoryApplication.java
@@ -9,17 +9,20 @@ import org.apache.http.client.HttpClient;
 
 public class InventoryApplication extends Application<InventoryConfiguration> {
 
-  @Override /* We can put things into the bootstrap before our own run code is called. */
+
+
+  public static void main(String[] args) throws Exception {
+    new InventoryApplication().run(args); //invoke the DW run method
+  }
+
+  @Override /* Add commands to the bootstrap before our own run() code is called. */
   public void initialize(Bootstrap<InventoryConfiguration> bootstrap) {
     bootstrap.addCommand(new HttpClientCommand());
   }
 
-  public static void main(String[] args) throws Exception {
-    new InventoryApplication().run(args);
-  }
-
   @Override /* This is a template method called indirectly by the run(String[]) method */
   public void run(InventoryConfiguration config, Environment environment) {
+    // Instantiate and register remaining server components
     environment.healthChecks().register("db-health", new DBHealthCheck());
 
     final HttpClient httpClient = new HttpClientBuilder(environment)
