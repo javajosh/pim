9ae62114cbe8b1cd3a86e17b2046252ba93cb62d
JavaJosh <github@javajosh.com>
2021-11-08 23:43:05 -0500

    Get the Angular dev server to work with the Java backend dev server by using  a proxy via webpack and proxy.conf.json

--- pim-client-angular/src/app/app.component.ts
+++ pim-client-angular/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Client';
+
+  ngOnInit(): void {
+    console.log('Hello from the pim-client-angular AppComponent!');
+    console.log('Lets see if the backend is working');
+    fetch('http://localhost:4200/api/item/1',).then(res => console.log(res))
+  }
 }
