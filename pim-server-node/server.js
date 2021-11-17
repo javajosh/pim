b2f0f6597a2db74cb94afdde0bbdd7c9b444c770
JavaJosh <github@javajosh.com>
2021-11-16 21:35:39 -0500

    Cleanup the contentType code n server.js

--- pim-server-node/server.js
+++ pim-server-node/server.js
@@ -2,44 +2,42 @@ import { createServer } from 'http';
 import { WebSocketServer } from 'ws';
 import { readFile } from 'fs';
 import { extname } from  'path'
+import { getProp } from "./simpatico/core.js";
 
 
 const port = 2000;
 
 const server = createServer(function (request, response) {
-  console.log(`GET http://127.0.0.1:${port}/`);
-  // I want to know more about this request. Here is a
-  // good place to set a debugger, then run in debug
+  console.log(`${request.method} http://127.0.0.1:${port}/`);
 
   let filePath = '.' + request.url;
   if (filePath === './') filePath = './index.html';
 
   const contentTypes = {
-    '.html':'text/html',
-    '.js':'text/javascript',
-    '.css':'text/css',
-    '.json':'application/json',
-    '.png':'image/png',
-    '.jpg':'image/jpg',
-    '.wav':'audio/wav',
+    'html':'text/html',
+    'js'  :'text/javascript',
+    'css' :'text/css',
+    'json':'application/json',
+    'png' :'image/png',
+    'jpg' :'image/jpg',
+    'wav' :'audio/wav',
+    'svg' :'image/svg+xml',
+    'ico' :'image/x-icon',
   };
-  const ext = extname(filePath);
-  let contentType = contentTypes[ext] ? contentTypes[ext] : 'text/html';
+  const responseCodesFromErrorCodes = {
+    'ENOENT': 404,
+  };
 
   readFile(filePath, function (error, responseContent =  "something bad happened") {
-    let responseCode;
-    const responseCodesFromErrorCodes = {
-      'ENOENT': 404
-    };
-
+    const responseCode = error ? getProp(responseCodesFromErrorCodes, error.code, 500) : 200;
     if (error) {
-      responseCode = responseCodesFromErrorCodes[error.code] ? responseCodesFromErrorCodes[error.code]: 500;
-      readFile(`./${responseCode}.html`, (err, content) => {
-        responseContent = content
+      readFile(`./${responseCode}.html`, (err, fileContent) => {
+        if (err) throw `Unable to read ./${responseCode}.html`;
+        responseContent = fileContent;
       });
-    } else {
-      responseCode = 200;
     }
+    const ext = extname(filePath).slice(1);
+    const contentType = getProp(contentTypes, ext, 'text/html');
     response.writeHead(responseCode, {'Content-Type': contentType});
     response.end(responseContent, 'utf-8');
   });
@@ -52,10 +50,10 @@ const wss = new WebSocketServer({server});
 // Later we'll add PKI and point-to-point fanout
 const conns = [];
 wss.on('connection', conn => {
-  console.log('CONNECTION %s %s', conn.url, conn.protocol);
+  console.log('wss.onconnection => {conn:{url:%s, protocol:%s}}', conn.url, conn.protocol);
   conns.push(conn);
   conn.on('message', msg => {
-    console.log('MESSAGE %s', msg);
+    console.log('wss.conn.onmessage msg %s', msg.toString());
     conns.forEach(c => {
       if (c && c.readyState === 1){
         // if (c !== conn) // don't send to the sender.
